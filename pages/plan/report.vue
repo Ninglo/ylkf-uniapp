6bff502b9bf98dbd5db431b13bda1d78f694a73f
Chalorate <1359225795@qq.com>
2020-12-01 15:07:21 +0800

    1201
    格式化

--- pages/plan/report.vue
+++ pages/plan/report.vue
@@ -29,26 +29,30 @@
 					<image class="ll" src="../../static/later.png" mode="aspectFit"></image>
 					<image src="../../static/llater.png" mode="aspectFit"></image>
 				</view>
-				
-					<view class="content">
-								<view class="user-img"><open-data type="userAvatarUrl"></open-data></view>
-								<view class="user-name"><open-data type="userNickName"></open-data></view>
-								<text class="date">{{ date }}</text>
-								<view class="divide-line"></view>
-								<view class="course-num-time">
-									<text space="emsp">课程      用时</text>
-								</view>
-								<view class="course-num-time">
-									<text class="course-num">{{courseNum}}</text>
-									<text class="course-time" space="emsp">节   {{courseTime}}</text>
-								</view>
-								<view class="course-list" v-for="item in course">
-												<image style="width: 50rpx; height: 50rpx;" src="../../static/finish.png"></image>
-												<view class="finish">完成{{item.name}}课程</view>
-											</view>
-							</view>
-					
-				
+
+				<view class="content">
+					<view class="user-img">
+						<open-data type="userAvatarUrl"></open-data>
+					</view>
+					<view class="user-name">
+						<open-data type="userNickName"></open-data>
+					</view>
+					<text class="date">{{ date }}</text>
+					<view class="divide-line"></view>
+					<view class="course-num-time">
+						<text space="emsp">课程 用时</text>
+					</view>
+					<view class="course-num-time">
+						<text class="course-num">{{courseNum}}</text>
+						<text class="course-time" space="emsp">节 {{courseTime}}</text>
+					</view>
+					<view class="course-list" v-for="item in course">
+						<image style="width: 50rpx; height: 50rpx;" src="../../static/finish.png"></image>
+						<view class="finish">完成{{item.name}}课程</view>
+					</view>
+				</view>
+
+
 			</view>
 		</view>
 	</view>
@@ -59,145 +63,159 @@
 		data() {
 			return {
 				courseNum: 3,
-								courseTime: "03:27:39",
-								course: [
-									{
-										name: "课程1"
-									},
-									{
-										name: "课程2"
-									}
-								]
+				courseTime: "03:27:39",
+				course: [{
+						name: "课程1"
+					},
+					{
+						name: "课程2"
+					}
+				]
 			}
 		},
 		methods: {
-			
+
 		}
 	}
 </script>
 
 <style>
-	.big-data-box{
+	.big-data-box {
 		display: flex;
 		justify-content: space-evenly;
 		border-bottom: #C0C0C0 solid 1rpx;
 		margin: 20rpx;
 		padding-bottom: 20rpx;
-		
+
 	}
-	.data-box{
+
+	.data-box {
 		width: 200rpx;
 		height: 125rpx;
 		text-align: center;
 		display: inline-block;
 	}
-	.dt-title{
+
+	.dt-title {
 		position: relative;
 		top: 20rpx;
 		font-size: 25rpx;
 	}
-	.dt{
+
+	.dt {
 		position: relative;
 		top: 30rpx;
 		font-size: 25rpx;
 	}
-	.dt text{
+
+	.dt text {
 		color: #ff00ff;
 		font-size: 40rpx;
 	}
-	.middle text{
+
+	.middle text {
 		padding: 10rpx;
 	}
-	.data-view{
+
+	.data-view {
 		background-color: #C0C0C0;
 		height: 400rpx;
 	}
-	
-	.choose-time{
-		background-color:#d1d3ce ;
+
+	.choose-time {
+		background-color: #d1d3ce;
 		text-align: center;
 	}
-	.choose-time image{
+
+	.choose-time image {
 		width: 60rpx;
 		height: 60rpx;
 		margin-left: 20rpx;
 	}
-	.choose-time .ll{
+
+	.choose-time .ll {
 		width: 40rpx;
 	}
-	.foot{
-		
+
+	.foot {
+
 		padding: 20rpx;
 		position: relative;
 		top: 10rpx;
 		border: #C0C0C0 solid 1rpx;
 	}
-	.foot text{
+
+	.foot text {
 		text-align: left;
 	}
-	.time{
+
+	.time {
 		display: inline-block;
 		position: relative;
 		bottom: 14rpx;
 		margin-left: 60rpx;
 		margin-right: 60rpx;
 	}
-	.record-box{
+
+	.record-box {
 		width: 90%;
 		border: #C0C0C0 solid 1rpx;
 		position: relative;
 		left: 5%;
 		margin: 10rpx;
 	}
-		.content {
-			width: 100%;
-		}
-		
-		.user-img {
-			margin: 5% 10%;
-			width: 150rpx;
-			height: 150rpx;
-			display: inline-block;
-			border-radius: 50%;
-		}
-	
-		.user-name {
-			display: inline-block;
-			position: relative;
-			bottom: 100rpx;
-		}
-		
-		.date {
-			position: relative;
-			right: 70rpx;
-		}
-		
-		.divide-line {
-			background: #E0E3DA;
-			width: 80%;
-			height: 5rpx;
-			position: relative;
-			left: 10%;
-			top: 10rpx;
-		}
-		
-		.course-num-time {
-			margin: 20rpx 10%;
-		}
-		
-		.course-num, .course-time {
-			color: purple;
-			font-weight: bold;
-			font-size: 50rpx;
-		}
-		.course-list {
-				margin: 15rpx 30rpx 10rpx 30rpx;
-				height: 60rpx;
-			}
-			
-			.finish {
-				margin-left: 80rpx;
-				position: relative;
-				bottom: 60rpx;
-			}
+
+	.content {
+		width: 100%;
+	}
+
+	.user-img {
+		margin: 5% 10%;
+		width: 150rpx;
+		height: 150rpx;
+		display: inline-block;
+		border-radius: 50%;
+	}
+
+	.user-name {
+		display: inline-block;
+		position: relative;
+		bottom: 100rpx;
+	}
+
+	.date {
+		position: relative;
+		right: 70rpx;
+	}
+
+	.divide-line {
+		background: #E0E3DA;
+		width: 80%;
+		height: 5rpx;
+		position: relative;
+		left: 10%;
+		top: 10rpx;
+	}
+
+	.course-num-time {
+		margin: 20rpx 10%;
+	}
+
+	.course-num,
+	.course-time {
+		color: purple;
+		font-weight: bold;
+		font-size: 50rpx;
+	}
+
+	.course-list {
+		margin: 15rpx 30rpx 10rpx 30rpx;
+		height: 60rpx;
+	}
+
+	.finish {
+		margin-left: 80rpx;
+		position: relative;
+		bottom: 60rpx;
+	}
 </style>
